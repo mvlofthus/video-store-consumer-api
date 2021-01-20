3135ee3bf796cd59526e055a43b8ca1bb3d73012
Leah Scott-Zechlin <scottzec@gmail.com>
2021-01-20 15:36:25 -0800

    pseudocode add video to db

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,4 +1,6 @@
 class VideosController < ApplicationController
+  require 'httparty'
+
   before_action :require_video, only: [:show]
 
   def index
@@ -19,6 +21,36 @@ class VideosController < ApplicationController
         methods: [:available_inventory]
         )
       )
+  end
+
+  # ANSEL ADVICE
+  # Make use of video wrapper code to make call to TMDB
+  # Get it from rails application.
+  # In React user enters title and inventory.
+  # This talks to Rails via the axios
+  # Rails code then queries tmdb to get details for that movie
+  # Then add it into your database
+  # Router is just to selectively show or hide bits of the React app
+  # It amkes it look like there are different routes to visit, but there really aren't bc single page app
+  # web server that react runs is special bc requests sent into it get sent to react app running, regardless of path in address bar, anything coming in gets sent to index.js
+  #
+  #
+  # Is it get or post? Get from tmdb
+
+
+  # look up the title to get data
+  # add to db
+  # doesnt sound like enough to make a helperclass
+  # put it in an action in controller
+  # just make use of wrapper
+
+  def add_new_video.get(url, params)
+    SEE VIDEO WRAPPER
+    URL = "the url of tmdb --search the video"
+    response = HTTParty.get(url, query: params)
+
+    we format the response to have the info we want to add to the video table
+    then we .save to the video table
   end
 
   private
